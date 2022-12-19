e55f3a53cffcc94c16c2e76602a3751a97f2c4a7
ozcnii <ercount89@gmail.com>
2022-12-19 20:58:14 +0300

    [refactoring] added typescript to the Options/Body component

--- src/components/CenterBar/Options/Body.vue
+++ src/components/CenterBar/Options/Body.vue
@@ -8,91 +8,103 @@
   />
 </template>
 
-<script>
-import { defineComponent } from "vue";
-import { mapGetters, mapMutations } from "vuex";
-import { Mutations } from "./../../../store.old/mutations";
+<script lang="ts" setup>
+import { onMounted, ref, watch } from "vue";
+
 import { VAceEditor } from "vue3-ace-editor";
+// @ts-ignore
+import ace from "ace-builds";
+// @ts-ignore
+import workerJsonUrl from "ace-builds/src-noconflict/worker-json?url";
+
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-tomorrow_night_bright";
 
-import ace from "ace-builds";
-import workerJsonUrl from "ace-builds/src-noconflict/worker-json?url";
+import { useFoldersStore } from "@/store/folders";
+import { MyNotActiveRequest, MyRequest, useRequestStore } from "@/store/request";
+import { stat } from "fs";
+
 ace.config.setModuleUrl("ace/mode/json_worker", workerJsonUrl);
 
-export default defineComponent({
-  components: {
-    VAceEditor,
-  },
+const foldersStore = useFoldersStore();
+const requestStore = useRequestStore();
 
-  mounted() {
-    const body = this.getActiveRequest?.body;
-    if (body) this.body = body;
-    else {
-      const body = this.getNotActiveRequest?.body;
-      try {
-        if (body) this.body = JSON.stringify(JSON.parse(body), null, this.jsonTabSize);
-      } catch (error) {
-        if (body) this.body = body;
-      }
+const JSON_TAB_SIZE = 2;
+const body = ref("{}");
+
+onMounted(() => {
+  const requestBody =
+    requestStore.activeRequest?.body || requestStore.notActiveRequest?.body;
+
+  if (requestBody) {
+    try {
+      body.value = JSON.stringify(JSON.parse(requestBody), null, JSON_TAB_SIZE);
+    } catch {
+      body.value = requestBody;
+    }
+  }
+});
+
+function saveBody() {
+  if (!requestStore.activeRequest) {
+    return;
+  }
+
+  const newRequest: MyRequest = {
+    ...requestStore.activeRequest,
+    body: body.value,
+  };
+
+  foldersStore.saveRequest(newRequest);
+  requestStore.setActiveRequest(newRequest);
+}
+
+function saveNotActiveRequestBody() {
+  if (!requestStore.notActiveRequest) {
+    return;
+  }
+
+  const newRequest: MyNotActiveRequest = {
+    ...requestStore.notActiveRequest,
+    body: body.value,
+  };
+
+  requestStore.setNotActiveRequest(newRequest);
+}
+
+watch(
+  body,
+  () => {
+    if (requestStore.activeRequest) {
+      saveBody();
+    }
+
+    const notActiveRequestBody = JSON.stringify(requestStore.notActiveRequest?.body);
+    const currentBody = JSON.stringify(body.value);
+
+    if (notActiveRequestBody !== currentBody) {
+      saveNotActiveRequestBody();
     }
   },
+  {
+    deep: true,
+  }
+);
 
-  data() {
-    return {
-      body: "{}",
-      jsonTabSize: 2,
-    };
-  },
+requestStore.$subscribe((_, state) => {
+  // params.value = state.activeRequest?.params || [];
 
-  methods: {
-    saveBody() {
-      const newRequest = {
-        ...this.getActiveRequest,
-        body: this.body,
-      };
+  const requestBody = state.activeRequest?.body;
 
-      this[Mutations.SAVE_REQUEST](newRequest);
-      this[Mutations.SET_ACTIVE_REQUEST](newRequest);
-    },
+  if (!requestBody) {
+    body.value = "{}";
+    return;
+  }
 
-    saveNotActiveRequestBody() {
-      const request = { ...this.getNotActiveRequest, body: this.body };
-      this[Mutations.SET_NOT_ACTIVE_REQUEST](request);
-    },
-
-    ...mapMutations([
-      `${Mutations.SAVE_REQUEST}`,
-      `${Mutations.SET_ACTIVE_REQUEST}`,
-      `${Mutations.SET_NOT_ACTIVE_REQUEST}`,
-    ]),
-  },
-
-  watch: {
-    body: {
-      handler() {
-        if (this.getActiveRequest) this.saveBody();
-        else this.saveNotActiveRequestBody();
-      },
-    },
-    getActiveRequest: {
-      handler(value) {
-        const body = value?.body;
-        if (body) this.body = body;
-        try {
-          if (body) this.body = JSON.stringify(JSON.parse(body), null, this.jsonTabSize);
-        } catch (error) {
-          if (body) this.body = body;
-        } finally {
-          if (!body) this.body = "{}";
-        }
-      },
-      deep: true,
-    },
-  },
-
-  computed: {
-    ...mapGetters(["getActiveRequest", "getNotActiveRequest"]),
-  },
+  try {
+    body.value = JSON.stringify(JSON.parse(requestBody), null, JSON_TAB_SIZE);
+  } catch {
+    body.value = requestBody;
+  }
 });
 </script>
